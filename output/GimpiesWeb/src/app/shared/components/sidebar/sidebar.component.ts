156c48780cc15d7e73dac94e3f47b2010cddde6c
mohamedloul <loulmohamed346@gmail.com>
2025-06-16 08:30:49 +0200

    add profil picture to the sidebar

--- output/GimpiesWeb/src/app/shared/components/sidebar/sidebar.component.ts
+++ output/GimpiesWeb/src/app/shared/components/sidebar/sidebar.component.ts
@@ -9,6 +9,12 @@ export class SidebarComponent implements OnInit {
 
   @Input() collapsed = false;
   @Output() closeSidebar = new EventEmitter<void>();
+  logout(): void {
+    // 👉 Hier kun je logica toevoegen om in de toekomst echt uit te loggen
+    console.log('Logging out...');
+    // Bijvoorbeeld navigeren naar loginpagina:
+    // this.router.navigate(['/login']);
+  }
 
   constructor() {}
 
